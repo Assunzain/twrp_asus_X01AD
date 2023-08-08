fce716357b78736990d30d8d93a38196655e1dc7
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2023-08-09 03:13:05 +0700

    X01AD: add flags pbrp 12.1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,8 @@ TW_HAS_EDL_MODE := true
 TW_USE_TOOLBOX := true
 TW_SKIP_COMPATIBILITY_CHECK := true
 TW_CUSTOM_CPU_TEMP_PATH := /sys/devices/virtual/thermal/thermal_zone3/temp
+PB_DISABLE_DEFAULT_DM_VERITY := true
+PB_TORCH_PATH := "/sys/class/leds/led:torch_1"
 
 
 # Debug
