aaf2042f9f2794099cd38653e684a0dc69ea728d
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-02-25 22:48:31 +0700

    Bringup SHRP12.1

--- twrp_X01AD.mk
+++ twrp_X01AD.mk
@@ -18,9 +18,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
-
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Charger
 PRODUCT_PACKAGES += \
