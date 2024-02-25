0cd4afbb59184d141f71b670e99512178a7db3c6
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-02-25 22:53:53 +0700

    Bringup SHRP 12.1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,12 +130,39 @@ TW_EXCLUDE_PYTHON := true
 TW_INCLUDE_RESETPROP := true
 TW_DEVICE_VERSION := 0_Zain
 
+#SHRP VARIABLES 
+# Path of your SHRP Tree
+SHRP_PATH := device/asus/X01AD
+# Maintainer name *
+SHRP_MAINTAINER := AssunZain
+# Device codename *
+SHRP_DEVICE_CODE := X01AD
+# Recovery Type (It can be treble,normal,SAR) [Only for About Section] *
+SHRP_REC_TYPE := SAR
+# Recovery Type (It can be A/B or A_only) [Only for About Section] *
+SHRP_DEVICE_TYPE := A_Only
+
+SHRP_STATUSBAR_LEFT_PADDING := 40
+SHRP_STATUSBAR_RIGHT_PADDING := 40
+SHRP_NOTCH := true
+SHRP_DARK := true
+SHRP_EXTERNAL := /external_sd
+SHRP_INTERNAL := /sdcard
+SHRP_OTG := /usb_otg
+SHRP_FLASH := 0 
+SHRP_CUSTOM_FLASHLIGHT := true
+SHRP_FONP_1 := /sys/class/leds/led:torch_1/brightness
+SHRP_FONP_2 := /sys/class/leds/led:torch_0/brightness
+SHRP_FONP_3 := /sys/class/leds/led:switch_1/brightness
+SHRP_FLASH_MAX_BRIGHTNESS := 200
+SHRP_EDL_MODE := 1
+SHRP_REC := /dev/block/bootdevice/by-name/recovery
+INC_IN_REC_MAGISK := true
+SHRP_OFFICIAL := false
+
 # Debug
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
-
-# Disable Mouse Cursor
-TW_INPUT_BLACKLIST := "hbtp_vm"
 
 # exFAT FS Support
 TW_INCLUDE_FUSE_EXFAT := true
