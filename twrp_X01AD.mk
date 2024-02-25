3864e14892a6040d6587d64cd34a0e544f087e6c
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-02-26 05:21:15 +0700

    Shrp

--- twrp_X01AD.mk
+++ twrp_X01AD.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Charger
 PRODUCT_PACKAGES += \
