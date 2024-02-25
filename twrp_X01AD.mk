5b95ed18e4523db3e6aa5c1d085875f3a8b2332c
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-02-25 13:15:33 +0700

    X01AD: Bringup Orangefox

--- twrp_X01AD.mk
+++ twrp_X01AD.mk
@@ -18,9 +18,8 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
-
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Charger
 PRODUCT_PACKAGES += \
