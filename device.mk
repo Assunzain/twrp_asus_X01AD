41f18148d8a97aabb28faaba4c01da5a0094b9ed
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-02-25 09:27:45 +0700

    X01AD: add product device

--- device.mk
+++ device.mk
@@ -5,3 +5,18 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.fastbootd.available=true
+
+# Shipping API Level
+PRODUCT_SHIPPING_API_LEVEL := 27
+
+# Crypto
+PRODUCT_PACKAGES += \
+    qcom_decrypt \
+    qcom_decrypt_fbe
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    keymaster_ver=3.0
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
