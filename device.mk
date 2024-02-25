7f65e3b6732901ab1237601db4d25ab21b4f8c8d
Azep Sunanda Zain <58516381+Assunzain@users.noreply.github.com>
2024-02-25 17:11:05 +0700

    X01AD: Add keymaster@4.0

--- device.mk
+++ device.mk
@@ -9,8 +9,13 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Shipping API Level
 PRODUCT_SHIPPING_API_LEVEL := 27
 
+# Keymaster
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.0-impl
+
 PRODUCT_PROPERTY_OVERRIDES += \
-    keymaster_ver=3.0
+    keymaster_ver=3.0 \
+    keymaster_ver=4.0
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
